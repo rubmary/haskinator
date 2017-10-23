b89dfccf4486a1bf4feef9092f58c7f76ef81971
rubmary <rubmaryrojas@gmail.com>
2017-10-23 15:06:19 -0400

    Pregunta critica

---
+++ PreguntaCritica.hs
@@ -0,0 +1,35 @@
+module Main(main) where
+import Oraculo
+import Data.Map as M
+import Prelude as P
+
+
+data Lca =  Lca String String String | First | Second | None
+
+
+lca :: String -> String -> Oraculo -> Lca
+lca s1 s2 (Prediccion prediccion)
+    | prediccion == s1  = First
+    | prediccion == s2  = Second
+    | otherwise         = None
+lca s1 s2 oraculo =
+    case P.filter noNone listaLcas of
+        []          -> None
+        [(x, op)]   -> x
+        [(First,  op1), (Second, op2)]  -> Lca (pregunta oraculo) op1 op2
+        [(Second, op2), (First,  op1)]  -> Lca (pregunta oraculo) op1 op2
+    where
+        lcaAux (a, b) = (lca s1 s2 b, a)
+        listaLcas = P.map lcaAux (toList (opciones oraculo))
+        noNone (None, _) = False
+        noNone _ = True 
+
+preguntaCritica :: String -> String -> Oraculo -> [String]
+preguntaCritica s1 s2 oraculo =
+    case (lca s1 s2 oraculo) of
+        Lca p op1 op2   -> [p, op1, op2]
+        First           -> ["1"]
+        Second          -> ["2"]
+        otherwise       -> [] 
+
+main = putStrLn "hola"
