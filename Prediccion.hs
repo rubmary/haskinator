fe95b3eaf2d33acadacf30a543efccbd338ce77b
rubmary <rubmaryrojas@gmail.com>
2017-10-23 17:09:02 -0400

    Restructuracion posicion de funciones

--- Prediccion.hs
+++ Prediccion.hs
@@ -4,52 +4,11 @@ import Data.Map as M
 import Prelude as P
 
 
------------  Interacciones con el usuario --------------
-obtenerRespuestaValida :: [String] -> IO String
-obtenerRespuestaValida opciones = do
-    respuesta <- getLine
-    case (elem respuesta opciones) of
-        True  -> return respuesta
-        False -> do
-            putStrLn "Opcion invalida, intente de nuevo!"
-            obtenerRespuestaValida opciones
 
-
-pedirPrediccionCorrecta :: IO String
-pedirPrediccionCorrecta = do
-    putStrLn "He fallado! Cuál era la respuesta correcta?"
-    getLine
-
-pedirPregunta :: String -> IO String
-pedirPregunta prediccionCorrecta = do 
-    putStrLn $ "Que pregunta distingue a " ++ prediccionCorrecta 
-                ++ " de las otras opciones?"
-    getLine
-
-pedirRespuesta :: String -> String -> IO String
-pedirRespuesta pregunta prediccion = do 
-    putStrLn $ "Cuál es la respuesta a \"" ++ pregunta ++ 
-                "\" para " ++ prediccion ++ "?"
-    getLine
-------------------------------------------------------
-
-
+--------------------------------  Predicciones ---------------------------------------
 predecir :: Oraculo -> IO Oraculo
-predecir (Prediccion p)       = predecirPrediccion (Prediccion p)
+predecir (Prediccion p)    = predecirPrediccion (Prediccion p)
 predecir (Pregunta p ops)  = predecirPregunta (Pregunta p ops)
-
-
-listaOpciones :: Opciones -> [String]
-listaOpciones ops = P.map fst (toList ops)
-
-showOpciones :: [String] -> String
-showOpciones ops = P.foldl concatenar (head ops) (tail ops)
-    where concatenar a b = a ++ " / " ++ b
-
-
-insertar :: String -> Oraculo -> Oraculo -> Oraculo
-insertar op prediccion (Pregunta p ops)  = Pregunta p (insert op prediccion ops)
-insertar _ _ _= error ("El oraculo no es una pregunta")
 
 
 predecirPregunta :: Oraculo -> IO Oraculo
@@ -87,9 +46,52 @@ prediccionFallida oraculo = do
     return ( ramificar  [opcionPrediccionOraculo, opcionPrediccionCorrecta]
                         [oraculo, crearOraculo prediccionCorrecta]
                         pregunta )
-            
 
------------------------- Main -----------------
+
+
+-------------------------  Interacciones con el usuario ------------------------
+listaOpciones :: Opciones -> [String]
+listaOpciones ops = P.map fst (toList ops)
+
+showOpciones :: [String] -> String
+showOpciones ops = P.foldl concatenar (head ops) (tail ops)
+    where concatenar a b = a ++ " / " ++ b
+
+
+insertar :: String -> Oraculo -> Oraculo -> Oraculo
+insertar op prediccion (Pregunta p ops)  = Pregunta p (insert op prediccion ops)
+insertar _ _ _= error ("El oraculo no es una pregunta")
+
+
+---------------------- Cambiar de ubicacion esta funcion -------------------------
+obtenerRespuestaValida :: [String] -> IO String
+obtenerRespuestaValida opciones = do
+    respuesta <- getLine
+    case (elem respuesta opciones) of
+        True  -> return respuesta
+        False -> do
+            putStrLn "Opcion invalida, intente de nuevo!"
+            obtenerRespuestaValida opciones
+
+pedirPrediccionCorrecta :: IO String
+pedirPrediccionCorrecta = do
+    putStrLn "He fallado! Cuál era la respuesta correcta?"
+    getLine
+
+pedirPregunta :: String -> IO String
+pedirPregunta prediccionCorrecta = do
+    putStrLn $ "Que pregunta distingue a " ++ prediccionCorrecta
+                ++ " de las otras opciones?"
+    getLine
+
+pedirRespuesta :: String -> String -> IO String
+pedirRespuesta pregunta prediccion = do
+    putStrLn $ "Cuál es la respuesta a \"" ++ pregunta ++
+                "\" para " ++ prediccion ++ "?"
+    getLine
+
+
+------------------------------------- Pruebas ----------------------------------
 
 oraculoInicial = Pregunta "Es un lenguaje de programaci\243n?" (fromList [("No",Prediccion "HTML"),("Si",Pregunta "A qu\233 paradigma pertenece?" (fromList [("Funcional",Prediccion "Haskell"),("Imperativo",Pregunta "A qui\233n pertenece el lenguaje?" (fromList [("Microsoft",Prediccion "C#"),("Oracle",Prediccion "Java")])),("L\243gico",Prediccion "Prolog")]))])
 
--- PreguntaCritica.hs
+++ PreguntaCritica.hs
@@ -11,17 +11,19 @@ lca s1 s2 (Prediccion prediccion)
     | prediccion == s1  = First
     | prediccion == s2  = Second
     | otherwise         = None
-lca s1 s2 oraculo =
-    case P.filter noNone listaLcas of
-        []          -> None
-        [(x, op)]   -> x
-        [(First,  op1), (Second, op2)]  -> Lca (pregunta oraculo) op1 op2
-        [(Second, op2), (First,  op1)]  -> Lca (pregunta oraculo) op1 op2
-    where
-        lcaAux (a, b) = (lca s1 s2 b, a)
-        listaLcas = P.map lcaAux (toList (opciones oraculo))
-        noNone (None, _) = False
-        noNone _ = True 
+lca s1 s2 oraculo = case s1 == s2 of
+    True -> error("Las predicciones no pueden ser iguales")
+    otherwise ->
+        case P.filter noNone listaLcas of
+            []          -> None
+            [(x, op)]   -> x
+            [(First,  op1), (Second, op2)]  -> Lca (pregunta oraculo) op1 op2
+            [(Second, op2), (First,  op1)]  -> Lca (pregunta oraculo) op1 op2
+        where
+            lcaAux (a, b) = (lca s1 s2 b, a)
+            listaLcas = P.map lcaAux (toList (opciones oraculo))
+            noNone (None, _) = False
+            noNone _ = True
 
 preguntaCritica :: String -> String -> Oraculo -> [String]
 preguntaCritica s1 s2 oraculo =
@@ -29,6 +31,15 @@ preguntaCritica s1 s2 oraculo =
         Lca p op1 op2   -> [p, op1, op2]
         First           -> ["1"]
         Second          -> ["2"]
-        otherwise       -> [] 
+        otherwise       -> []
 
-main = putStrLn "hola"
+------------------------------------- PRUEBAS ------------------------------
+
+oraculoInicial = Pregunta "Es un lenguaje de programaci\243n?" (fromList [("No",Prediccion "HTML"),("Si",Pregunta "A qu\233 paradigma pertenece?" (fromList [("Funcional",Prediccion "Haskell"),("Imperativo",Pregunta "A qui\233n pertenece el lenguaje?" (fromList [("Microsoft",Prediccion "C#"),("Oracle",Prediccion "Java")])),("L\243gico",Prediccion "Prolog")]))])
+main = do
+    putStrLn "Escribir primera prediccion"
+    s1 <- getLine
+    putStrLn "Escribir segunda prediccion"
+    s2 <- getLine
+    putStrLn $ show (preguntaCritica s1 s2 oraculoInicial)
+    main
