066c7056dc844c4d88088d5046f36617482a2b31
Carlos Infante <carlosinfante1220@gmail.com>
2017-10-23 19:27:30 -0400

    Predicción

--- Haskinator.hs
+++ Haskinator.hs
@@ -14,6 +14,7 @@ menu oraculoActual = do
         choice <- getLine
         oraculoNuevo <- case read choice of
             1 -> crearNuevo
+            2 -> predecir oraculoActual
             3 -> persistir oraculoActual
             4 -> cargar
             5 -> obtenerPreguntaCritica oraculoActual
@@ -99,6 +100,91 @@ mostrarPreguntaCritica (p:op1:op2:_) s1 s2 = do
     putStrLn ("La respuesta para " ++ s1 ++ " es " ++ op1)
     putStrLn ("La respuesta para " ++ s2 ++ " es " ++ op2)
 mostrarPreguntaCritica (err:_) _ _ = putStrLn err
+
+--------------------------------  Predicciones ---------------------------------------
+predecir :: Oraculo -> IO Oraculo
+predecir (Prediccion p)    = predecirPrediccion (Prediccion p)
+predecir (Pregunta p ops)  = predecirPregunta (Pregunta p ops)
+
+
+predecirPregunta :: Oraculo -> IO Oraculo
+predecirPregunta oraculo = do 
+    putStrLn $ pregunta oraculo
+    putStrLn $ showOpciones $ (listaOpciones $ opciones oraculo)
+    usuario <- obtenerRespuestaValida ("ninguna": (listaOpciones (opciones oraculo)))
+    case usuario of
+        "ninguna" -> do
+            prediccionCorrecta <- pedirPrediccionCorrecta
+            opcion <- pedirRespuesta (pregunta oraculo) prediccionCorrecta
+            return (insertar opcion (crearOraculo prediccionCorrecta) oraculo)
+        opcion -> do
+            subOraculo <- predecir (respuesta oraculo opcion)
+            return (insertar opcion subOraculo oraculo)
+
+
+
+predecirPrediccion :: Oraculo -> IO Oraculo
+predecirPrediccion oraculo = do
+    putStrLn $ "Prediccion: " ++ (prediccion oraculo)
+    putStrLn $ showOpciones opcionesSiNo
+    x <- obtenerRespuestaValida opcionesSiNo
+    case x of
+        "Si" -> return oraculo
+        "No" -> prediccionFallida oraculo
+    where opcionesSiNo = ["Si", "No"]
+
+prediccionFallida :: Oraculo -> IO Oraculo
+prediccionFallida oraculo = do
+    prediccionCorrecta <- pedirPrediccionCorrecta
+    pregunta <- (pedirPregunta prediccionCorrecta)
+    opcionPrediccionCorrecta  <- (pedirRespuesta pregunta prediccionCorrecta)
+    opcionPrediccionOraculo   <- (pedirRespuesta pregunta (prediccion oraculo))
+    return ( ramificar  [opcionPrediccionOraculo, opcionPrediccionCorrecta]
+                        [oraculo, crearOraculo prediccionCorrecta]
+                        pregunta )
+
+
+
+-------------------------  Interacciones con el usuario ------------------------
+listaOpciones :: Opciones -> [String]
+listaOpciones ops = P.map fst (toList ops)
+
+showOpciones :: [String] -> String
+showOpciones ops = P.foldl concatenar (head ops) (tail ops)
+    where concatenar a b = a ++ " / " ++ b
+
+
+insertar :: String -> Oraculo -> Oraculo -> Oraculo
+insertar op prediccion (Pregunta p ops)  = Pregunta p (insert op prediccion ops)
+insertar _ _ _= error ("El oraculo no es una pregunta")
+
+
+---------------------- Cambiar de ubicacion esta funcion -------------------------
+obtenerRespuestaValida :: [String] -> IO String
+obtenerRespuestaValida opciones = do
+    respuesta <- getLine
+    case (elem respuesta opciones) of
+        True  -> return respuesta
+        False -> do
+            putStrLn "Opcion invalida, intente de nuevo!"
+            obtenerRespuestaValida opciones
+
+pedirPrediccionCorrecta :: IO String
+pedirPrediccionCorrecta = do
+    putStrLn "He fallado! Cuál era la respuesta correcta?"
+    getLine
+
+pedirPregunta :: String -> IO String
+pedirPregunta prediccionCorrecta = do
+    putStrLn $ "Que pregunta distingue a " ++ prediccionCorrecta
+                ++ " de las otras opciones?"
+    getLine
+
+pedirRespuesta :: String -> String -> IO String
+pedirRespuesta pregunta prediccion = do
+    putStrLn $ "Cuál es la respuesta a \"" ++ pregunta ++
+                "\" para " ++ prediccion ++ "?"
+    getLine
 
 main::IO()
 main = menu $ crearOraculo ""
