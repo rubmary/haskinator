96e1e9af9856b191dab80bcebf7a3673c63f4bc3
Carlos Infante <carlosinfante1220@gmail.com>
2017-10-24 22:04:48 -0400

    Comentarios añadidos. :sheep:

--- Haskinator.hs
+++ Haskinator.hs
@@ -1,3 +1,12 @@
+{-
+Módulo  : Haskinator
+Autores : Rubmary Rojas 13-11264, Carlos Infante 13-10681
+
+Programa principal Haskinator. Presenta un menú de opciones
+al usuario. Le permite realizar predicciones sobre un Oraculo
+existente y modificarlo, o empezar un Oraculo desde una primera
+Pediccion.
+-}
 module Main(main) where
 import Oraculo
 import System.Exit
@@ -5,8 +14,16 @@ import System.IO
 import Data.Map as M
 import Prelude as P
 
+-- Representa la respuesta para el ancestro común más bajo.
+-- Si es Lca, contiene tres cadenas de caracteres con la respuesta.
+-- Si es First, solo existe la primera opción.
+-- Si es Second, solo existe la segunda opción.
+-- Si es None, no existe ninguna opción.
 data Lca =  Lca String String String | First | Second | None
 
+-- Muestra al usuario una serie de opciones numeradas, mantiene un Oraculo
+-- que se usará para las predicciones, y avisa al usuario cuando
+-- introduce una opción incorrecta.
 menu :: Oraculo -> IO()
 menu oraculoActual = do
         putStrLn "Introduzca una opción."
@@ -25,7 +42,7 @@ menu oraculoActual = do
         menu oraculoNuevo
     where showChoices (i, s) = show i ++ ". " ++ s
 
-
+-- Opciones numeradas para el menú.
 choices :: [(Int, String)]
 choices = zip [1.. ] [
         "Crear un oráculo nuevo",
@@ -36,12 +53,16 @@ choices = zip [1.. ] [
         "Salir"
     ]
 
+-- Pide al usuario una cadena de caracteres y crea un
+-- nuevo Oraculo incluyendo solo una Prediccion.
 crearNuevo :: IO Oraculo
 crearNuevo = do
     putStrLn "Introduzca una predicción."
     pred <- getLine
     return $ crearOraculo pred    
 
+-- Pide al usuario que introduzca la ruta de un archivo
+-- y crea un Oraculo con su contenido.
 cargar :: IO Oraculo
 cargar = do
         putStrLn "Introduzca el archivo a leer."
@@ -51,6 +72,9 @@ cargar = do
     where
         oraculo s = read s :: Oraculo
 
+
+-- Recibe un Oraculo, pide al usuario que introduzca un nombre
+-- de archivo, y guarda el Oraculo en el archivo.
 persistir :: Oraculo -> IO Oraculo
 persistir oraculo = do
     putStrLn "Introduzca el archivo para guardar el oráculo."
@@ -58,6 +82,10 @@ persistir oraculo = do
     writeFile archivo (show oraculo)
     return $ oraculo
 
+--------------------------------  Pregunta crucial -------------------------------------
+
+-- Recibe dos cadenas de caracteres que representan predicciones
+-- y un Oraculo, y encuentra su ancestro común más bajo en el Oraculo.
 lca :: String -> String -> Oraculo -> Lca
 lca s1 s2 (Prediccion prediccion)
     | prediccion == s1  = First
@@ -77,6 +105,9 @@ lca s1 s2 oraculo = case s1 == s2 of
             noNone (None, _) = False
             noNone _ = True
 
+-- Recibe dos cadenas de caracteres que representan predicciones
+-- y un Oraculo, aplica lca y transforma la respuesta del tipo Lca
+-- a una lista de cadenas de caracteres con la respuesta o un error.
 preguntaCritica :: String -> String -> Oraculo -> [String]
 preguntaCritica s1 s2 oraculo =
     case (lca s1 s2 oraculo) of
@@ -85,6 +116,9 @@ preguntaCritica s1 s2 oraculo =
         Second          -> ["La primera opción no existe."]
         otherwise       -> ["Ninguna de las opciones existe."]
 
+-- Recibe dos predicciones del usuario, y busca sobre el Oraculo
+-- actual la pregunta crucial que las separa. Simplemente devuelve
+-- una advertencia en caso de que alguna predicción no exista.
 obtenerPreguntaCritica :: Oraculo -> IO Oraculo
 obtenerPreguntaCritica oraculo = do
     putStrLn "Escribir primera prediccion"
@@ -94,6 +128,8 @@ obtenerPreguntaCritica oraculo = do
     mostrarPreguntaCritica (preguntaCritica s1 s2 oraculo) s1 s2
     return oraculo
 
+-- Muestra la pregunta crítica al usario, y las opciones que llevan
+-- a cada Prediccion.
 mostrarPreguntaCritica :: [String] -> String -> String -> IO()
 mostrarPreguntaCritica (p:op1:op2:_) s1 s2 = do
     putStrLn ("La pregunta crucial es " ++ p)
@@ -102,11 +138,15 @@ mostrarPreguntaCritica (p:op1:op2:_) s1 s2 = do
 mostrarPreguntaCritica (err:_) _ _ = putStrLn err
 
 --------------------------------  Predicciones ---------------------------------------
+
+-- Inicia o continúa una secuencia de predicción.
 predecir :: Oraculo -> IO Oraculo
 predecir (Prediccion p)    = predecirPrediccion (Prediccion p)
 predecir (Pregunta p ops)  = predecirPregunta (Pregunta p ops)
 
-
+-- Se plantea al usuario una pregunta y sus opciones. Si el usuario responde
+-- "ninguna", se le pide al usuario la opción que esperaba y su respuesta.
+-- De otra forma, la predicción continúa hacia el sub-oráculo necesario.
 predecirPregunta :: Oraculo -> IO Oraculo
 predecirPregunta oraculo = do 
     putStrLn $ pregunta oraculo
@@ -122,7 +162,8 @@ predecirPregunta oraculo = do
             return (insertar opcion subOraculo oraculo)
 
 
-
+-- Se plantea al usuario una predicción, y este debe decidir si es correcta.
+-- En caso negativo, se le pide la opción correcta y una pregunta que permita discernirla.
 predecirPrediccion :: Oraculo -> IO Oraculo
 predecirPrediccion oraculo = do
     putStrLn $ "Prediccion: " ++ (prediccion oraculo)
@@ -133,6 +174,8 @@ predecirPrediccion oraculo = do
         "No" -> prediccionFallida oraculo
     where opcionesSiNo = ["Si", "No"]
 
+-- Se le pide el usuario la predicción correcta y la pregunta para llegar a ella.
+-- Se modifica el oráculo para incluir la nueva información.
 prediccionFallida :: Oraculo -> IO Oraculo
 prediccionFallida oraculo = do
     prediccionCorrecta <- pedirPrediccionCorrecta
@@ -143,48 +186,69 @@ prediccionFallida oraculo = do
                         [oraculo, crearOraculo prediccionCorrecta]
                         pregunta )
 
-
-
--------------------------  Interacciones con el usuario ------------------------
-listaOpciones :: Opciones -> [String]
-listaOpciones ops = P.map fst (toList ops)
-
-showOpciones :: [String] -> String
-showOpciones ops = P.foldl concatenar (head ops) (tail ops)
-    where concatenar a b = a ++ " / " ++ b
-
-
+-- Inserta una nueva opción para una pregunta.
 insertar :: String -> Oraculo -> Oraculo -> Oraculo
 insertar op prediccion (Pregunta p ops)  = Pregunta p (insert op prediccion ops)
 insertar _ _ _= error ("El oraculo no es una pregunta")
 
+-------------------------  Interacciones con el usuario ------------------------
 
----------------------- Cambiar de ubicacion esta funcion -------------------------
+-- Verifica que la respuesta del usuario a una pregunta sea válida.
+-- Para que sea válida, la respuesta debe formar parte de las Opciones.
 obtenerRespuestaValida :: [String] -> IO String
 obtenerRespuestaValida opciones = do
     respuesta <- getLine
     case (elem respuesta opciones) of
         True  -> return respuesta
         False -> do
-            putStrLn "Opcion invalida, intente de nuevo!"
+            putStrLn "Opcion inválida, intente de nuevo!"
             obtenerRespuestaValida opciones
 
+-- Pide al usuario la opción correcta.
 pedirPrediccionCorrecta :: IO String
 pedirPrediccionCorrecta = do
     putStrLn "He fallado! Cuál era la respuesta correcta?"
     getLine
 
+-- Pide al usuario la pregunta que distinga la opción correcta.
 pedirPregunta :: String -> IO String
 pedirPregunta prediccionCorrecta = do
     putStrLn $ "Que pregunta distingue a " ++ prediccionCorrecta
                 ++ " de las otras opciones?"
     getLine
 
+-- Pide la respuesta a la pregunta para una predicción.
 pedirRespuesta :: String -> String -> IO String
 pedirRespuesta pregunta prediccion = do
     putStrLn $ "Cuál es la respuesta a \"" ++ pregunta ++
                 "\" para " ++ prediccion ++ "?"
-    getLine
+    obtenerOpcionValida
 
+-- Verifica que la opción introducida por el usuario sea correcta.
+-- "ninguna" no es una opción válida pues está reservada para que
+-- el usuario agregue una opción adicional.
+obtenerOpcionValida :: IO String
+obtenerOpcionValida = do
+    opcion <- getLine
+    case opcion of
+        "ninguna" -> do
+            putStrLn "\"ninguna\" no puede ser respuesta. Intente de nuevo."
+            obtenerOpcionValida
+        _ -> do
+            return opcion
+
+-- Recibe Opciones y devuelve la lista de cadenas de caracteres
+-- asociada con ellas.
+listaOpciones :: Opciones -> [String]
+listaOpciones ops = P.map fst (toList ops)
+
+-- Muestra la lista de cadena de caracteres asociada con la lista de opciones.
+showOpciones :: [String] -> String
+showOpciones ops = P.foldl concatenar (head ops) (tail ops)
+    where concatenar a b = a ++ " / " ++ b
+
+
+
+-- Ejecuta el menú con un primer oráculo vacío.
 main::IO()
 main = menu $ crearOraculo ""
--- Oraculo.hs
+++ Oraculo.hs
@@ -1,25 +1,49 @@
+{-
+Módulo  : Oraculo
+Autores : Rubmary Rojas 13-11264, Carlos Infante 13-10681
+
+Módulo incluyendo las definiciones para los tipos de datos
+y sus funciones de creación, acceso y modificación.
+-}
 module Oraculo where
 import Data.Map as M
 
+-- Una Opcion es un diccionario que contiene informacion de la opción escogida
+-- en la pregunta anterior y cómo continuar con la predicción.
 type Opciones = Map String Oraculo
+
+-- Un Oraculo puede ser una Pregunta o una Prediccion.
+-- Una Prediccion contiene una cadena de caracteres.
+-- Una Pregunta contiene una cadena de caracteres y una serie de
+-- opciones como respuesta.
 data Oraculo = Pregunta String Opciones | Prediccion String 
                 deriving(Show,Read)
 
+-- Recibe una cadena de caracteres y devuelve un Oraculo como Prediccion.
 crearOraculo :: String -> Oraculo
 crearOraculo s = Prediccion s
 
+-- Recibe un Oraculo y devuelve su cadena de caracteres si es una Prediccion.
+-- Arroja error si el Oraculo no es una Prediccion.
 prediccion :: Oraculo -> String
 prediccion (Prediccion s) = s
 prediccion _ = error "No es una predicción."
 
+-- Recibe un Oraculo y devuelve su cadena de caracteres si es una Pregunta.
+-- Arroja error si el Oraculo no es una Pregunta.
 pregunta :: Oraculo -> String
 pregunta (Pregunta s _) = s
 pregunta _ = error "No es una pregunta."
 
+-- Recibe un Oraculo y devuelve sus Opciones si es una Pregunta.
+-- Arroja error si el Oraculo no es una Pregunta.
 opciones :: Oraculo -> Opciones
 opciones (Pregunta _ opciones) = opciones
 opciones _ = error "No es una pregunta."
 
+-- Recibe un Oraculo y una cadena de caracteres y devuelve el Oraculo
+-- asociado a la cadena, si es una Pregunta.
+-- Arroja error si el Oraculo no es una Pregunta.
 respuesta :: Oraculo -> String -> Oraculo
 respuesta (Pregunta _ opciones) key =
     case (M.lookup key opciones) of
@@ -27,6 +51,9 @@ respuesta (Pregunta _ opciones) key =
         Nothing -> error "No es una respuesta válida."
 respuesta _ _ = error "No es una pregunta."
 
+-- Recibe una lista de cadenas de caracteres, una lista de Oraculos,
+-- y una cadena de caracteres. Devuelve un Oraculo de tipo Pregunta
+-- usando la cadena como pregunta y las listas como Opciones.
 ramificar :: [String] -> [Oraculo] -> String -> Oraculo
 ramificar opciones oraculos pregunta =
     Pregunta pregunta (fromList (zip opciones oraculos))
